bf661ac4aa51219f18d72e74d521e35d4e531d68
Arukala Himateja <himateja1014@gmail.com>
2023-10-19 02:11:07 -0500

    models and configurations

---
+++ Food-delivery-app-deliverable-1-code/src/main/java/com/fda/app/config/GeolocationException.java
@@ -0,0 +1,7 @@
+package com.fda.app.config;
+
+public class GeolocationException extends RuntimeException{
+    public GeolocationException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ Food-delivery-app-deliverable-1-code/src/main/java/com/fda/app/model/GLocation.java
@@ -0,0 +1,113 @@
+package com.fda.app.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fda.app.config.CustomDateAndTimeDeserialize;
+import com.fda.app.config.CustomJsonDateSerializer;
+import com.fda.app.constants.Constants;
+
+@Entity
+@Table(name = Constants.GLOCATION_TABLE_NAME)
+@JsonIgnoreProperties
+public class GLocation {
+
+	public static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	@CreatedDate
+	@JsonSerialize(using = CustomJsonDateSerializer.class)
+	@JsonDeserialize(using = CustomDateAndTimeDeserialize.class)
+	private Date createdAt;
+
+	@Column(nullable = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	@CreatedDate
+	@JsonSerialize(using = CustomJsonDateSerializer.class)
+	@JsonDeserialize(using = CustomDateAndTimeDeserialize.class)
+	private Date updatedAt;
+
+	private Long restaurantId;
+	private Double locationLat;
+	private Double locationLng;
+	private String address;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public Long getRestaurantId() {
+		return restaurantId;
+	}
+
+	public void setRestaurantId(Long restaurantId) {
+		this.restaurantId = restaurantId;
+	}
+
+	public Double getLocationLat() {
+		return locationLat;
+	}
+
+	public void setLocationLat(Double locationLat) {
+		this.locationLat = locationLat;
+	}
+
+	public Double getLocationLng() {
+		return locationLng;
+	}
+
+	public void setLocationLng(Double locationLng) {
+		this.locationLng = locationLng;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
