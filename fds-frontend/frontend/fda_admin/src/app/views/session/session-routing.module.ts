221f6235a96eeb3d3af95faf83b283851e46078c
yashaswini26 <yashaswinireddy2606@gmail.com>
2023-11-10 16:10:09 -0600

    signin and session module

--- fds-frontend/frontend/fda_admin/src/app/views/session/session-routing.module.ts
+++ fds-frontend/frontend/fda_admin/src/app/views/session/session-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 const routes: Routes = [
   { path: 'signin', component: SigninComponent },
+  { path: 'forgotpwd', component: ForgotPasswordComponent },
 ];
 
 @NgModule({
