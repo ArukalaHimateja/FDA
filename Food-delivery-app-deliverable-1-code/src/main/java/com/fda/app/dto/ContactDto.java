6a494a0c423b7f75c708b402a496e37addef25f6
praneel mallu <praneelreddymallu@gmail.com>
2023-11-11 03:05:53 -0600

    user

---
+++ Food-delivery-app-deliverable-1-code/src/main/java/com/fda/app/dto/ContactDto.java
@@ -0,0 +1,50 @@
+package com.fda.app.dto;
+
+public class ContactDto {
+	private String name;
+	private String email;
+	private String mobileNumber;
+	private String subject;
+	private String message;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
