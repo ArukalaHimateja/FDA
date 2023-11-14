cdb3ec4f2c62595eac52d61766f16c5a917d51e0
Arukala Himateja <Himateja1014@gmail.com>
2023-11-13 19:44:30 -0600

    Reviews and Promo code models and change in webSecurity config

---
+++ Food-delivery-app-deliverable-1-code/src/main/java/com/fda/app/model/PromoCode.java
@@ -0,0 +1,148 @@
+package com.fda.app.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fda.app.config.CustomDateAndTimeDeserialize;
+import com.fda.app.config.CustomJsonDateSerializer;
+import com.fda.app.constants.Constants;
+
+@Entity
+@Table(name = Constants.PROMO_CODE_TABLE_NAME)
+@JsonIgnoreProperties
+public class PromoCode {
+
+	public static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	@CreatedDate
+	@JsonSerialize(using = CustomJsonDateSerializer.class)
+	@JsonDeserialize(using = CustomDateAndTimeDeserialize.class)
+	private Date createdAt;
+
+	@Column(nullable = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	@CreatedDate
+	@JsonSerialize(using = CustomJsonDateSerializer.class)
+	@JsonDeserialize(using = CustomDateAndTimeDeserialize.class)
+	private Date updatedAt;
+
+	private String type;
+	private String description;
+	private Date startDate;
+	private Date endDate;
+	private int value;
+	private String promoCode;
+	private Boolean status = Boolean.TRUE;
+	private Long restaurantId;
+
+	public Long getRestaurantId() {
+		return restaurantId;
+	}
+
+	public void setRestaurantId(Long restaurantId) {
+		this.restaurantId = restaurantId;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public String getPromoCode() {
+		return promoCode;
+	}
+
+	public void setPromoCode(String promoCode) {
+		this.promoCode = promoCode;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public Boolean getStatus() {
+		return status;
+	}
+
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
