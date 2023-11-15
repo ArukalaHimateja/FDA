d01d934eff0a148b6fecf448959a2b7f084c579d
yashaswini26 <yashaswinireddy2606@gmail.com>
2023-11-15 00:45:23 -0600

    feedback components

--- fds-frontend/frontend/fda_admin/src/app/views/admin/feedback/feedback.component.ts
+++ fds-frontend/frontend/fda_admin/src/app/views/admin/feedback/feedback.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Pagination } from 'src/app/shared/interfaces/pagination.interface';
+import { UtilityService } from 'src/app/shared/services/utility.service';
+import { AuthService } from '../../session/auth.service';
+import { OrderService } from '../order/order.service';
+import { FeedbackService } from './feedback.service';
 
 @Component({
   selector: 'app-feedback',
@@ -6,5 +12,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./feedback.component.scss']
 })
 export class FeedbackComponent {
+  displayedColumns: string[] = [];
+  dataSource: any;
+  pagination: Pagination;
+  pageNumber: any = 1;
+  sessionUser: any;
+
+  constructor(
+    public _utilityService: UtilityService,
+    private _authService: AuthService,
+    private _feedbackService: FeedbackService,
+  ) {
+    this.displayedColumns = this._feedbackService.displayedColumns;
+    this.pagination = _utilityService.pagination;
+    this.sessionUser = this._authService.getAuthUser();
+    this.getDataList();
+  }
+  
+  /**
+  * Get next page data
+  * 
+  * @param page 
+  */
+  getNextPageData(page: any) {
+    this.pageNumber = page.pageIndex + 1;
+    this.pagination.currentPage = page.pageIndex + 1;
+    this.getDataList();
+  }
+
+  /**
+   * Get Data List with pagination
+   */
+  getDataList() {
+    let json = {
+      pagination: this.pagination,
+    }
+    this._feedbackService.getFeedbackListByFilterWithPagination(json).then((response: any) => {
+      this.dataSource = new MatTableDataSource(response.body.data.data);
+    })
+  }
 
 }
