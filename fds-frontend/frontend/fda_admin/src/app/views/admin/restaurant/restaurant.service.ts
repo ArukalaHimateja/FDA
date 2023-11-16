b8bc5a69febcd6edaa021a174cbcbfe5353cea78
yashaswini26 <yashaswinireddy2606@gmail.com>
2023-11-16 01:32:39 -0600

    restaurant components, module and services

--- fds-frontend/frontend/fda_admin/src/app/views/admin/restaurant/restaurant.service.ts
+++ fds-frontend/frontend/fda_admin/src/app/views/admin/restaurant/restaurant.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../../../shared/services/api.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
@@ -31,8 +32,23 @@ export class RestaurantService {
      * 
      * @param id 
      */
-    getDataById(id: any) {
-        return this._apiService.get(`properties/${id}`);
+    getRestaurantDtlByRequestId(id: any) {
+        return this._apiService.get(`get/restaurant/request/${id}`);
     }
 
+
+    downloadDocumentByRestaurantRequestId(id: any) {
+        this._apiService.get(`get/restaurant/request/document/${id}`).then((response: any) => {
+            // this._apiService.get(`file/download/${response.body.data.document}`);
+            const data = response.body.data.document;
+            // this.isLoading = false;
+            const blob = new Blob([data], { type: 'application/zip' });
+            const url = window.URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = `${environment.apiUrl}file/download/${data}`;
+            link.setAttribute('download', `${environment.apiUrl}/file/download/${data}`);
+            document.body.appendChild(link);
+            link.click();
+        })
+    }
 }
