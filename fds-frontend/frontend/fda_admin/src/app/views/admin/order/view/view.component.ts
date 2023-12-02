5e68e3b1e406ae471aded0cb5d037762fed8d0de
yashaswini26 <yashaswinireddy2606@gmail.com>
2023-12-02 02:56:29 -0600

    View order components

--- fds-frontend/frontend/fda_admin/src/app/views/admin/order/view/view.component.ts
+++ fds-frontend/frontend/fda_admin/src/app/views/admin/order/view/view.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { OrderService } from '../order.service';
+import { OrderProductDetailType, OrderDetailType } from 'src/app/shared/interfaces/pagination.interface';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-view',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./view.component.scss']
 })
 export class ViewComponent {
-
+  orderId!: number;
+  orderProductDetailList!: OrderProductDetailType[];
+  orderDetail!: OrderDetailType;
+  
+  constructor(
+    private _orderService: OrderService,
+    public dialogRef: MatDialogRef<ViewComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+  ) {
+    this._orderService.getSubOrderDetailByOrderId(this.data.orderId).then((response: any) => {
+      this.orderProductDetailList = response.body.data;
+    })
+    this._orderService.getOrderDetailByOrderId(this.data.orderId).then((response: any) => {
+      this.orderDetail = response.body.data;
+    })
+  }
 }
