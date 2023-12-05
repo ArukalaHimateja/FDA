0c8749dee69e8119e8931575919d769e78f4d1f3
praneel mallu <praneelreddymallu@gmail.com>
2023-12-04 18:09:44 -0600

    Authentication

--- Food-delivery-app-deliverable-1-code/src/test/java/com/fda/controller/BillingControllerTest.java
+++ Food-delivery-app-deliverable-1-code/src/test/java/com/fda/controller/BillingControllerTest.java
@@ -3,6 +3,9 @@ package com.fda.controller;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,17 +13,18 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.fda.app.FoodDeriveryAppApplication;
 import com.fda.app.dto.ApiResponseDto.ApiResponseDtoBuilder;
-import com.fda.app.dto.CheckoutPayment;
 import com.fda.app.dto.ContactDto;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT, classes = FoodDeriveryAppApplication.class)
-public class PaymentControllerTest {
+public class BillingControllerTest {
 	@LocalServerPort
 	private int port;
 
@@ -30,19 +34,31 @@ public class PaymentControllerTest {
 	private final String URL = "http://localhost:";
 
 	@Test
-	public void paymentForStripeProduct() throws Exception {
+	public void billingGenerate() throws Exception {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
-		CheckoutPayment checkoutPayment = new CheckoutPayment();
-		checkoutPayment.setCancelUrl("test");
-		checkoutPayment.setOrderId(1l);
-		checkoutPayment.setSuccessUrl("test");
-		String url = URL + port + "/api/stripe/payment";
-		HttpEntity<CheckoutPayment> request = new HttpEntity<>(checkoutPayment, headers);
 
-		ResponseEntity<ApiResponseDtoBuilder> responseEntity = restTemplate.postForEntity(url, request,
-				ApiResponseDtoBuilder.class);
+		Long orderId = 26l;
+		String url = URL + port + "/api/billing/generate";
+		HttpEntity<?> entity = new HttpEntity<>(headers);
+		String urlTemplate = UriComponentsBuilder.fromHttpUrl(url).queryParam("orderId", "{orderId}").encode()
+				.toUriString();
+		Map<String, Object> params = new HashMap<>();
+		params.put("orderId", orderId);
+		ResponseEntity<ApiResponseDtoBuilder> responseEntity = restTemplate.exchange(urlTemplate, HttpMethod.POST,
+				entity, ApiResponseDtoBuilder.class, params);
 		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+
 	}
 
+	@Test
+	public void getBillingById() throws Exception {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		String url = URL + port + "/api/get/billing/" + 26l;
+		ResponseEntity<ApiResponseDtoBuilder> responseEntity = restTemplate.getForEntity(url,
+				ApiResponseDtoBuilder.class);
+
+		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+	}
 }
