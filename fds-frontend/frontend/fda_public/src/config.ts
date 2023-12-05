b48adac28bd79628a62ee8afc5138ac70d02bc95
Rajapraful Kasarla <prafulrdy@gmail.com>
2023-12-05 00:03:56 -0600

    Packages

--- fds-frontend/frontend/fda_public/src/config.ts
+++ fds-frontend/frontend/fda_public/src/config.ts
@@ -1,5 +1,6 @@
 export const config = {
   apiUrl: 'http://159.65.157.19:8080/food/api/',
-  // apiUrl: 'http://192.168.1.8:8080/api/',
+  // apiUrl: 'http://192.168.1.9:8080/api/',
   // apiUrl: 'api/',
+  stripePublicKey:'pk_test_51NiFBGSHn8sLrTg2LPsdfsdfsdfd9h3FJMDEJV6iqGuj3yxIjNxGzZjPOECGn0hvH93csDnfAMbcOR5a2qrWvcjME3Bt9tyoNZ00TwV245Yq'
 }
